94ea94348e156f98d4a9c1e3a5d246d30ea835d1
Lokesh Patil <42104811+lokesh-ghoul@users.noreply.github.com>
2021-02-01 08:07:34 +0530

    Update base64.py

--- base64.py
+++ base64.py
@@ -29,54 +29,8 @@ def base64_encrypt(text):
         encypt += str(i)
     #encypt += '=='
     print(encypt)
-    '''
-def base64_decrypt(text1):
-    con = ''
-    #text1 = text1[:-2]
-    print(text1)
-    dbinary = []
-    for i in text1:
-        index = code.index(i)
-        #print(index)
-        binary = bin(index)
-        #print(binary)
-        binary = binary[2:]
-        #print(binary)
-        dbinary.append(binary)
-    print(dbinary)
-    for i in dbinary:
-        if len(i) != 6:
-            pz = 6 - len(i)
-            #print(pz)
-            loc = dbinary.index(i)
-            #print(loc)
-            zero=''
-            for n in range(0,pz):
-                zero += '0' 
-                #print(zero)
-            z = zero + i   
-            dbinary.insert(loc,z)     
-        else:
-            pass
-        #print(i)
-    #print(dbinary)
-
-    #print(dbinary)
-        con = con + binary  
-    print(con)    
-    n = 8
-    chunks = [con[i:i+n] for i in range(0, len(con), n)]  
-    #print(chunks)  
-    decrypt = ''
-    for i in chunks:  
-        i = int(i,2)
-        print(i)
-        i = chr(i)
-        decrypt += str(i)
-    #print(decrypt)
-'''
+   
 if __name__ == '__main__':
     text = input("Enter the string to encrypt:\t")
     base64_encrypt(text)
-    #text1 = input("Enter the string to decrypt:\t")
-    #base64_decrypt(text1)
+  
