307d3ce2258858979ae543c632cf232d7758d728
Lokesh Patil <42104811+lokesh-ghoul@users.noreply.github.com>
2021-02-02 11:43:13 +0530

    Update base64.py

--- base64.py
+++ base64.py
@@ -1,17 +1,18 @@
 global code
 code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
+                                # Encryption Function
 def base64_encrypt(text):
     con = ''
     for i in text :
-        asc = ord(i)                        # Ascii Conversion
-        binary = bin(asc)                   # Binary Conversions
-        binary = binary[2:]                 # removing '0b'
-        con =  con + '0' + binary                  #Concatining all
+        asc = ord(i)                                            # ASCII COnversion
+        binary = bin(asc)                                       # Binary Conversion
+        binary = binary[2:]                 
+        con =  con + '0' + binary                               # Concatenate all
     n = 6
-    chunks = [con[i:i+n] for i in range(0, len(con), n)]
-    for i in chunks:
-        if len(i) != 6:
+    chunks = [con[i:i+n] for i in range(0, len(con), n)]        # Grourping into 6 Char
+    for i in chunks:                                            # If any grp has less thn 6 char add additional required 00
+        if len(i) != 6:                             
             pz = 6 - len(i) 
             loc = chunks.index(i) 
             zero=''
@@ -22,14 +23,16 @@ def base64_encrypt(text):
         else:
             pass
     encypt = ''
-    for i in chunks:
-        i = '00' + i
-        i = int(i,2)
-        i = code[i]
+    for i in chunks:                                            
+        i = '00' + i                                            # Convert six-bit bytes into eight-bit bytes, by prepend the prefix “00”
+        i = int(i,2)                                            # Decimal COnversion
+        i = code[i]                                             # Base64 Chart Char COnversion
         encypt += str(i)
     #encypt += '=='
     print(encypt)
     
+    
+                                    # Decryption Function
 def base64_decrypt(text1):
     con = ''
     #text1 = text1[:-2]
@@ -64,6 +67,7 @@ def base64_decrypt(text1):
             decrypt += str(i)
     print(decrypt)
     
+                                     # Main
 if __name__ == '__main__':
     text = input("Enter the string to encrypt:\t")
     base64_encrypt(text)
